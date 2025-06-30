a6cc8dc7a85b7b4f9f846b04936c7be90e913508
Penubhakam Sasidhar <sasidharpenubhakam@gmail.com>
2025-06-30 12:07:02 +0530

    commit

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -82,7 +82,7 @@ const Product = () => {
       <div className='mt-20'>
         <div className='flex'>
           <b className='border px-5 py-3 text-sm'>Description</b>
-          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+          <p className='border px-5 py-3 text-sm'>Reviews (12)</p>
         </div>
         <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
           <p>An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products, interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.</p>
