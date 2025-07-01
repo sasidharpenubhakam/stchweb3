e86ed6bbbbd4ec8547e3ae9bf868c32b3d6aa738
Penubhakam Sasidhar <sasidharpenubhakam@gmail.com>
2025-07-01 12:13:54 +0530

    Cart Debug 2

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
@@ -41,11 +41,12 @@ const Cart = () => {
           cartData.map((item, index) => {
 
             const productData = products.find((product) => product._id === item._id);
+            console.log("PRODUCT DATA", productData);
 
             return (
               <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
                 <div className=' flex items-start gap-6'>
-                  <img className='w-16 sm:w-20' src={productData.image[0]} alt="" />
+                  <img className='w-16 sm:w-20' src={productData?.image[0]} alt="" />
                   <div>
                     <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
                     <div className='flex items-center gap-5 mt-2'>
