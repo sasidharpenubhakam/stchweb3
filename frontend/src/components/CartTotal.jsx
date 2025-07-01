0eba6c8f2aaaaf4920de4ef63ed452626ab18c0a
Penubhakam Sasidhar <sasidharpenubhakam@gmail.com>
2025-07-01 11:53:16 +0530

    Cart debug 1

--- frontend/src/components/CartTotal.jsx
+++ frontend/src/components/CartTotal.jsx
@@ -11,6 +11,7 @@ const CartTotal = () => {
   const handleApplyCoupon = () => {
     const subtotal = getCartAmount();
     // Simple example: if coupon is "BUY2SAVE100" and subtotal is at least 999, discount is 100
+    console.log("Cart Data", couponCode, couponCode === 'prem123', subtotal, typeof(subtotal));
     if (couponCode === 'prem123' && subtotal >= 999) {
       setDiscount(100);
       setCouponApplied(true);
