0bb007284f13a2f5cf21b25fa55d0dc079b0e4ee
Mr. Sasidhar <161588398+sasidharpenubhakam@users.noreply.github.com>
2025-07-04 12:29:10 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -4,7 +4,7 @@ import userModel from "../models/userModel.js";
 import Stripe from 'stripe';
 import razorpay from 'razorpay';
 
-const currency = 'inr';
+const currency = 'INR';
 const deliveryCharge = 29;
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
