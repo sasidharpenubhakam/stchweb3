eb94a72c2d8b4eec34460bc4a0989529f32d82e8
Mr. Sasidhar <161588398+sasidharpenubhakam@users.noreply.github.com>
2025-07-04 11:36:51 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -2,7 +2,6 @@ import express from 'express'
 import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay, cancelOrder} from '../controllers/orderController.js'
 import adminAuth  from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
-import { cancelOrder } from '../controllers/orderController.js';
 
 
 const orderRouter = express.Router()
@@ -19,9 +18,7 @@ orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
 // User Feature 
 orderRouter.post('/userorders',authUser,userOrders)
 orderRouter.post('/cancel',authUser,cancelOrder)
-// Add this line after userOrders
-orderRouter.post('/cancel', authUser, cancelOrder);
-✅
+
 
 // verify payment
 orderRouter.post('/verifyStripe',authUser, verifyStripe)
