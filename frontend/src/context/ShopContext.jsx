840ae9c65a829bbd542c70fdb03181849932aba2
Mr. Sasidhar <161588398+sasidharpenubhakam@users.noreply.github.com>
2025-07-04 11:02:20 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -8,7 +8,7 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
 
     const currency = '₹';
-    const delivery_fee = 10;
+    const delivery_fee = 29;
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
