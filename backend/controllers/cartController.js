2a6809da12a847b41c119d999b0420e2d5f50fb6
Mr. Sasidhar <161588398+sasidharpenubhakam@users.noreply.github.com>
2025-07-17 16:59:28 +0530

    Update cartController.js

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -82,15 +82,15 @@ const applyCoupon = async(req, res) => {
         console.log("HERE,", coupon, subtotal);
 
         // GET10: 10% discount on subtotal
-        if (coupon === 'STCH10' && subtotal > 0) {
-            const discount = Math.round(subtotal * 0.10);
+         if (coupon === 'DBR200' && subtotal > 0) {
+             const discount = Math.round(subtotal * 0.10);
             return res.json({ success: true, discount });
     }
-         // GET10: 10% discount on subtotal
-        if (coupon === 'DBR200' && subtotal > 0) {
-            const discount = Math.round(subtotal * 0.10);
-            return res.json({ success: true, discount });
-    }
+    //      // GET10: 10% discount on subtotal
+    //     if (coupon === 'DBR200' && subtotal > 0) {
+    //         const discount = Math.round(subtotal * 0.10);
+    //         return res.json({ success: true, discount });
+    // }
 
 
         // STCHGETONEFREE: If there are 4 or more t-shirts, discount price of cheapest t-shirt
