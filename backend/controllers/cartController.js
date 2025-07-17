4efc2e4b571976bfa51d7b54b1af91d563aa6d67
Mr. Sasidhar <161588398+sasidharpenubhakam@users.noreply.github.com>
2025-07-17 22:08:40 +0530

    Update cartController.js

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -82,7 +82,7 @@ const applyCoupon = async(req, res) => {
         console.log("HERE,", coupon, subtotal);
 
         // GET10: 10% discount on subtotal
-         if (coupon === 'DBR200' && subtotal > 0) {
+         if (coupon === 'STCH10' && subtotal > 0) {
              const discount = Math.round(subtotal * 0.20);
             return res.json({ success: true, discount });
     }
